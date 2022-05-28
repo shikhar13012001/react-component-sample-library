b1ebd1f5259a3190bffbd959fb46e3b3c9463cd6
Shikhar <ishgupta2015@gmail.com>
2022-05-28 16:45:46 +0530

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
 
-// NEW
-import postcss from "rollup-plugin-postcss";
+//NEW
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const packageJson = require("./package.json");
 
@@ -24,20 +26,22 @@ export default [
       },
     ],
     plugins: [
+      // NEW
+      peerDepsExternal(),
+
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
 
       // NEW
-      postcss(), 
+      terser(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-
-    // NEW
     external: [/\.css$/],
   },
 ];
